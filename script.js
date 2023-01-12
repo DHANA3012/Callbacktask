77538b9bfb2593750c43e6cbd50bfa45f5fe3296
DHANA3012 <109264553+DHANA3012@users.noreply.github.com>
2023-01-12 17:58:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let timer=document.createElement("h1")
+
+setTimeout(()=>{
+    timer.innerHTML= "10"
+    console.log("10")
+},1000)
+setTimeout(()=>{
+    timer.innerHTML= "9"
+    console.log("9")
+},2000)
+setTimeout(()=>{
+    timer.innerHTML= "8"
+    console.log("8")
+},3000)
+setTimeout(()=>{
+    timer.innerHTML= "7"
+    console.log("7")
+},4000)
+setTimeout(()=>{
+    timer.innerHTML= "6"
+    console.log("6")
+},5000)
+setTimeout(()=>{
+    timer.innerHTML= "5"
+    console.log("5")
+},6000)
+setTimeout(()=>{
+    timer.innerHTML= "4"
+    console.log("4")
+},7000)
+setTimeout(()=>{
+    timer.innerHTML= "3"
+    console.log("3")
+},8000)
+setTimeout(()=>{
+    timer.innerHTML= "2"
+    console.log("2")
+},9000)
+setTimeout(()=>{
+    timer.innerHTML= "1"
+    console.log("1")
+},10000)
+setTimeout(()=>{
+    timer.innerHTML="Happy Independence Day!!!"
+    console.log("Happy Independence Day!!!")
+},11000)
+document.body.append(timer)
